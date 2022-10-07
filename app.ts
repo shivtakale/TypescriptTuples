51210b3f444af2f7da20aa978f86ccfb2bc15b7c
Sadashiv Takale <takalesr@gmail.com>
2022-10-07 06:06:07 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,34 @@
+let cl = console.log;
+//TUPles >> fixed length array
+
+
+let person:{name:string,age:number,hobbies:string[],role:[number,string]} = {
+    name:"jhon",
+    age: 37,
+    hobbies: ['cricket','coding',"cooking"],
+    role: [2, 'admin']
+}
+
+// person.role[2]= ''
+// person.role.push(10);
+// person.role.push('true')
+cl(person.role);
+
+
+
+// A tuple is a typed array with a pre-defined length and types for each index.
+// Tuples are great because they allow each element in the array to be a known type of value.
+// To define a tuple, specify the type of each element in the array:
+
+// define our tuple
+let ourTuple: [number, boolean, string];
+
+// initialize correctly
+ourTuple = [5, false, 'Coding God was here'];
+
+
+// define our tuple
+let ourTuple1: [number, boolean, string];
+
+// initialized incorrectly which throws an error
+// ourTuple1 = [false, 'Coding God was mistaken', 5];
